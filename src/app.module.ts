875421b1183ed3ceca8129e1fc601900dc7c4dff
Diego Fernández Barrera <bigomby@gmail.com>
2018-07-27 12:15:28 +0200

    :bug: Disable sync

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,6 @@ import { AuthModule } from './auth/auth.module';
       password: 'password',
       database: 'postgres',
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: true,
     }),
   ],
 })
